565adc95879c0aa83e15acfffd8f1d5eaf0c9c7c
shgrayfox <shgrayfox@mail.ru>
2018-10-29 22:45:50 +0300

    Добавлена л.р.1

---
+++ TP1/Source.cpp
@@ -0,0 +1,111 @@
+//Определить класс с именем АЕRОFLОТ, содержащий следующие поля : 
+// название пункта назначения рейса;
+// номер рейса; 
+// тип самолета.
+
+//Определить методы доступа к этим полям и перегруженные операции извлечения и вставки для объектов типа АЕRОFLОТ.
+
+//Написать программу, выполняющую следующие действия : 
+// ввод с клавиатуры данных в массив, состоящий из семи объектов типа АЕRОFLОТ; 
+
+//записи должны быть упорядочены по возрастанию номера рейса;
+
+// вывод на экран номеров рейсов и типов самолетов, вылетающих в пункт назначения, название которого совпало с названием, введенным с клавиатуры; 
+// если таких рейсов нет, выдать на дисплей соответствующее сообщение.
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <fstream>
+using namespace std;
+
+class Aeroflot {
+private:
+	string to;
+	string number;
+	string type;
+public:
+	string getTo() {
+		return to;
+	}
+	string getNumber() {
+		return number;
+	}
+	string getType() {
+		return type;
+	}
+	/*string setTo(string t){
+	to = t;
+	}
+	string setNumber(string n) {
+	number = n;
+	}
+	string setType(string t) {
+	type = t;
+	}*/
+	void operator++() {// добавление нового элемента префикс
+		cout << "Создание нового рейса:\nВведите пункт назначения: ";
+		cin >> to;
+		cout << "Введите номер рейса: ";
+		cin >> number;
+		cout << "Введите тип самолета: ";
+		cin >> type;
+	}
+
+	void show(int curr_n) {// вывод данных
+		printf("  | %-16s | %-11s | %-12s | ", to.c_str(), number.c_str(), type.c_str());
+		printf("\n");
+	}
+};
+
+void main(void) {
+	Aeroflot aero[7];
+	Aeroflot *reis = aero;
+	int req, exit;
+	int cur = 0;
+	string current_to;
+	system("chcp 1251>nul");
+request:
+	cout << "Введите номер пункта меню:" << endl << "1: создать спсок рейсов;" << endl << "2: Показать общий список;" << endl << "3: Показать список рейсов для заданного пункта назначения;" << endl << "0: Завершение работы" << endl;
+	cin >> req;
+	switch (req) {
+	case 0: // завершение работы
+		return;
+	case 1: //++
+		for (int i = 0; i < 7; ++i)
+			++aero[i];
+		goto request;
+	case 2: // вывод общего списка 
+		cout << "  | Пункт назначения | Номер рейса | Тип самолета |" << endl;
+		for (int i = 0; i < 7; ++i)
+			(&aero[i])->show(i);
+		goto request;
+	case 3: // список рейсов для заданного пункта назначения
+		cout << "Введите пункт назначения: ";
+		cin >> current_to;
+		cout << "Список рейсов для заданного пункта назначения:" << endl << endl;
+		cur = 0;
+		for (int i = 0; i < 7; ++i)
+			if (((&aero[i])->getTo()) == current_to) {
+				(&aero[i])->show(i);
+				cur++;
+			}
+		if (cur == 0)
+			cout << "Рейсы не найдены" << endl << endl;
+		goto request;
+	default:
+		goto error;
+	}
+error:
+	cout << "Ошибка ввода.\nДля возвращения в главное меню введите 1, для завершения работы введите 0:" << endl;
+	cin >> exit;
+	switch (exit) {
+	case 1:
+		goto request;
+	case 0:
+		return;
+	default:
+		goto error;
+	}
+	system("pause>nul");
+}
